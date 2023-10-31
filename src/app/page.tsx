45a9fd7ac12d7b07c872983fa3c1ee7ac3a7feee
Nabarun <nicknabarun@gmail.com>
2023-10-31 19:42:40 +0530

    Minor style fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { buttonVariants } from "@/components/ui/Button";
 export default function Home() {
   return (
     <>
-      <MaxWidthWrapper className='flex flex-col items-center justify-center text-center mb-12 mt-28 sm:mt-40'>
+      <MaxWidthWrapper className='flex flex-col items-center justify-center text-center mb-12 mt-24 md:mt-28 lg:mt-32 xl:mt-36'>
         <div className='mx-auto flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white shadow-md backdrop-blur transition-all hover:border-gray-300 mb-4 px-7 py-2'>
           <p className='text-sm font-semibold text-gray-700 cursor-default'>
             Documon is now live!
