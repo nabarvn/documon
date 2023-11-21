c4c17e4dd34e507c9a36c809d16fa314e5039be8
Nabarun <nicknabarun@gmail.com>
2023-11-21 22:56:17 +0530

    Implement middleware to be utilized by a private procedure

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,6 +1,27 @@
-import { initTRPC } from "@trpc/server";
+import { TRPCError, initTRPC } from "@trpc/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const t = initTRPC.create();
+const middleware = t.middleware;
+
+const isAuth = middleware(async (opts) => {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
+  // guard clause
+  if (!user || !user.id) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return opts.next({
+    // context allows us to pass values directly to the api endpoint that uses this middleware via the procedure
+    ctx: {
+      user,
+      userId: user.id,
+    },
+  });
+});
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
+export const privateProcedure = t.procedure.use(isAuth);
