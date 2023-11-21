992ec412461b4431ee5327741309bce5dc64e235
Nabarun <nicknabarun@gmail.com>
2023-11-21 22:50:23 +0530

    Export `Dashboard` and `UploadButton`

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as MaxWidthWrapper } from "./MaxWidthWrapper";
 export { default as Navbar } from "./Navbar";
 export { default as Providers } from "./Providers";
+export { default as Dashboard } from "./Dashboard";
+export { default as UploadButton } from "./UploadButton";
