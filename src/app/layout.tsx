384df53de0738e4a34b63db21dd0eb70c1061929
Nabarun <nicknabarun@gmail.com>
2023-11-21 22:48:14 +0530

    Import react skeleton css module

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import { cn, constructMetadata } from "@/lib/utils";
 import { Navbar, Providers } from "@/components";
 
+import "react-loading-skeleton/dist/skeleton.css";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = constructMetadata();
