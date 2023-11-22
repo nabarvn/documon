2fed371d31a702ca8ae125e6455b8d42f593a073
Nabarun <nicknabarun@gmail.com>
2023-11-22 18:18:56 +0530

    Export `PdfRenderer`

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Providers } from "./Providers";
 export { default as Dashboard } from "./Dashboard";
 export { default as UploadButton } from "./UploadButton";
+export { default as PdfRenderer } from "./PdfRenderer";
