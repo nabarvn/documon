a9a73d49b8f4c305716d3d6f991c9c82cc9d03b7
Nabarun <nicknabarun@gmail.com>
2023-11-22 23:10:32 +0530

    Implement `Dropzone` with related business logic

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,8 +1,105 @@
 "use client";
 
 import { useState } from "react";
-import { Button } from "@/components/ui";
-import { Dialog, DialogTrigger } from "@/components/ui/Dialog";
+import Dropzone from "react-dropzone";
+import { Button, Progress } from "@/components/ui";
+import { Cloud, File, Loader2 } from "lucide-react";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/Dialog";
+
+const UploadDropzone = () => {
+  const [isUploading, setIsUploading] = useState<boolean>(false);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+
+  // creating a determinate progress bar
+  const startSimulatedProgress = () => {
+    setUploadProgress(0);
+
+    const interval = setInterval(() => {
+      setUploadProgress((prevProgress) => {
+        if (prevProgress >= 95) {
+          clearInterval(interval);
+          return prevProgress;
+        }
+
+        return prevProgress + 5;
+      });
+    }, 500);
+
+    return interval;
+  };
+
+  return (
+    <Dropzone
+      // we want users to drop only one file at a time
+      multiple={false}
+      onDrop={() => {
+        // TODO: business logic for pdf upload
+      }}
+    >
+      {({ getRootProps, getInputProps, acceptedFiles }) => (
+        <div
+          {...getRootProps()}
+          className='border h-64 border-dashed border-gray-300 rounded-lg m-4'
+        >
+          <div className='flex items-center justify-center h-full w-full'>
+            <label
+              htmlFor='dropzone-file'
+              className='flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'
+            >
+              <div className='flex flex-col items-center justify-center pt-5 pb-6'>
+                <Cloud className='h-6 w-6 text-zinc-500 mb-2' />
+
+                <p className='text-sm text-zinc-700 mb-2'>
+                  <span className='font-semibold'>Click to upload</span> or drag
+                  and drop.
+                </p>
+
+                <p className='text-xs text-zinc-500'>
+                  {/* TODO: calculate max size of pdf dynamically on the basis of subscription model */}
+                </p>
+              </div>
+
+              {acceptedFiles && acceptedFiles[0] ? (
+                <div className='max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200'>
+                  <div className='h-full grid place-items-center px-3 py-2'>
+                    <File className='h-4 w-4 text-blue-500' />
+                  </div>
+
+                  <div className='h-full text-sm truncate px-3 py-2'>
+                    {acceptedFiles[0].name}
+                  </div>
+                </div>
+              ) : null}
+
+              {isUploading ? (
+                <div className='w-full max-w-xs mx-auto mt-4'>
+                  <Progress
+                    value={uploadProgress}
+                    className='h-1 w-full bg-zinc-200'
+                  />
+
+                  {uploadProgress === 100 ? (
+                    <div className='flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2'>
+                      <Loader2 className='h-3 w-3 animate-spin' />
+                      Redirecting...
+                    </div>
+                  ) : null}
+                </div>
+              ) : null}
+
+              <input
+                {...getInputProps()}
+                type='file'
+                id='dropzone-file'
+                className='hidden'
+              />
+            </label>
+          </div>
+        </div>
+      )}
+    </Dropzone>
+  );
+};
 
 const UploadButton = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -19,6 +116,10 @@ const UploadButton = () => {
       <DialogTrigger onClick={() => setIsOpen(true)} asChild>
         <Button>Upload PDF</Button>
       </DialogTrigger>
+
+      <DialogContent>
+        <UploadDropzone />
+      </DialogContent>
     </Dialog>
   );
 };
