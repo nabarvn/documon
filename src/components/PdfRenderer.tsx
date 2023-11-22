54a2e1ca0db42fd8ef8975b45c7de422298359e3
Nabarun <nicknabarun@gmail.com>
2023-11-22 18:42:08 +0530

    Style the top bar `div` elements

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,5 +1,11 @@
 const PdfRenderer = () => {
-  return <div>PdfRenderer</div>;
+  return (
+    <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
+      <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
+        <div className='flex items-center gap-1.5'>{/* top bar */}</div>
+      </div>
+    </div>
+  );
 };
 
 export default PdfRenderer;
