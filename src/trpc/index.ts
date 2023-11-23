b9dcbae77486f56394e443c2c13a339c9802b41e
Nabarun <nicknabarun@gmail.com>
2023-11-23 19:57:32 +0530

    Add `getFile` route to retrieve a specific file

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -44,6 +44,23 @@ export const appRouter = router({
     });
   }),
 
+  getFile: privateProcedure
+    .input(z.object({ key: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await db.file.findFirst({
+        where: {
+          key: input.key,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      return file;
+    }),
+
   deleteFile: privateProcedure
     .input(z.object({ id: z.string() }))
     .mutation(async ({ ctx, input }) => {
