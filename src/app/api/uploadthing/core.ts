fa6947b03b74612fdd10ec5f3bc0e55adc999591
Nabarun <nicknabarun@gmail.com>
2023-11-23 20:05:12 +0530

    Add db integration and file creation on upload completion

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,3 +1,4 @@
+import { db } from "@/db";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
@@ -6,7 +7,7 @@ const f = createUploadthing();
 // `FileRouter` for the app, can contain multiple file routes
 export const ourFileRouter = {
   // one can define as many file routes as they would like, each with a unique `routeSlug`
-  pdfUploader: f({ image: { maxFileSize: "4MB" } })
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
     // set permissions and file types for this file route
     .middleware(() => {
       const { getUser } = getKindeServerSession();
@@ -17,8 +18,18 @@ export const ourFileRouter = {
       // whatever is returned here is accessible in `onUploadComplete` as `metadata`
       return { userId: user.id };
     })
-    .onUploadComplete(() => {
+    .onUploadComplete(async ({ metadata, file }) => {
       // this code RUNS ON SERVER after upload
+
+      const createdFile = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          userId: metadata.userId,
+          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+          uploadStatus: "PROCESSING",
+        },
+      });
     }),
 } satisfies FileRouter;
 
