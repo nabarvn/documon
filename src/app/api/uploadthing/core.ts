b55cfab6e8e98a0c188ec96db8579fe0d2346053
Nabarun <nicknabarun@gmail.com>
2023-11-23 17:51:23 +0530

    Initial commit

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,25 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+const f = createUploadthing();
+
+// `FileRouter` for the app, can contain multiple file routes
+export const ourFileRouter = {
+  // one can define as many file routes as they would like, each with a unique `routeSlug`
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    // set permissions and file types for this file route
+    .middleware(() => {
+      const { getUser } = getKindeServerSession();
+      const user = getUser();
+
+      if (!user || !user.id) throw new Error("Unauthorized");
+
+      // whatever is returned here is accessible in `onUploadComplete` as `metadata`
+      return { userId: user.id };
+    })
+    .onUploadComplete(() => {
+      // this code RUNS ON SERVER after upload
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
