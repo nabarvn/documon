c19ab8cbd7c22eab60c46eaff33957ca46bba058
Nabarun <nicknabarun@gmail.com>
2023-11-24 22:57:41 +0530

    Refactor `Progress` component to accept custom indicator color

--- src/components/ui/Progress.tsx
+++ src/components/ui/Progress.tsx
@@ -5,10 +5,16 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/lib/utils";
 
+type ProgressProps = React.ComponentPropsWithoutRef<
+  typeof ProgressPrimitive.Root
+> & {
+  indicatorColor?: string;
+};
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+  ProgressProps
+>(({ className, value, indicatorColor, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -18,7 +24,10 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className='h-full w-full flex-1 bg-primary transition-all'
+      className={cn(
+        "h-full w-full flex-1 bg-primary transition-all",
+        indicatorColor
+      )}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
