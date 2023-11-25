3a4abc8e4c766dbcacc222968e365222d9abb81e
Nabarun <nicknabarun@gmail.com>
2023-11-25 23:40:26 +0530

    Exclude canvas and encoding aliases from webpack resolution

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
