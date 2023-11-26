9fd225a4fe9f4ebaf2dde9558fcb70442cb2e6c4
Nabarun <nicknabarun@gmail.com>
2023-11-26 23:01:15 +0530

    Refactor `PdfRenderer` component for improved user experience

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Loader2 } from "lucide-react";
+import { useState } from "react";
+import { ChevronDown, Loader2 } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf";
 import { toast } from "@/components/ui/UseToast";
 
@@ -11,6 +12,7 @@ import "react-pdf/dist/Page/TextLayer.css";
 // worker code is needed to properly render the pdf file
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+import { Button, Input } from "@/components/ui";
 import { useResizeDetector } from "react-resize-detector";
 
 interface PdfRendererProps {
@@ -20,10 +22,25 @@ interface PdfRendererProps {
 const PdfRenderer = ({ url }: PdfRendererProps) => {
   const { width, ref } = useResizeDetector();
 
+  const [numPages, setNumPages] = useState<number>();
+
   return (
     <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
       <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
-        <div className='flex items-center gap-1.5'>{/* top bar */}</div>
+        <div className='flex items-center gap-1.5'>
+          <Button variant='ghost' aria-label='previous page'>
+            <ChevronDown className='h-4 w-4' />
+          </Button>
+
+          <div className='flex items-center gap-1.5'>
+            <Input className='w-12 h-8' />
+
+            <p className='text-zinc-700 text-sm space-x-1'>
+              <span>/</span>
+              <span>{numPages ?? "x"}</span>
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className='flex-1 w-full max-h-screen'>
@@ -43,6 +60,7 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
                 <Loader2 className='h-6 w-6 animate-spin my-24' />
               </div>
             }
+            onLoadSuccess={({ numPages }) => setNumPages(numPages)}
           >
             <Page
               width={width ? width : 1}
