d2680ef512050d5df54050218cd726087811331a
Nabarun <nicknabarun@gmail.com>
2023-11-30 23:57:53 +0530

    Add `getFileUploadStatus` endpoint

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -61,6 +61,21 @@ export const appRouter = router({
       return file;
     }),
 
+  getFileUploadStatus: privateProcedure
+    .input(z.object({ fileId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const file = await db.file.findFirst({
+        where: {
+          id: input.fileId,
+          userId: ctx.userId,
+        },
+      });
+
+      if (!file) return { status: "PENDING" as const };
+
+      return { status: file.uploadStatus };
+    }),
+
   deleteFile: privateProcedure
     .input(z.object({ id: z.string() }))
     .mutation(async ({ ctx, input }) => {
