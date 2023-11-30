c6eec85b6331194da4a6d1e112f64daf7b66e8af
Nabarun <nicknabarun@gmail.com>
2023-11-30 23:59:22 +0530

    Integrate file upload status into `ChatWrapper`

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -1,6 +1,21 @@
+import { trpc } from "@/app/_trpc/client";
 import { ChatInput, Messages } from "@/components/chat";
 
-const ChatWrapper = () => {
+interface ChatWrapperProps {
+  fileId: string;
+}
+
+const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
+  const { data, isLoading } = trpc.getFileUploadStatus.useQuery(
+    {
+      fileId,
+    },
+    {
+      refetchInterval: (data) =>
+        data?.status === "SUCCESS" || data?.status === "FAILED" ? false : 500,
+    }
+  );
+
   return (
     <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
       <div className='flex-1 justify-between flex flex-col mb-28'>
