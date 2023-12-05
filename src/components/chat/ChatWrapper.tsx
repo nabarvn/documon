71371ffd917fc484348bb2f4ae4176cf44a51447
Nabarun <nicknabarun@gmail.com>
2023-12-05 23:57:21 +0530

    Introduce `ChatContextProvider` to `ChatWrapper`

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -5,6 +5,7 @@ import { trpc } from "@/app/_trpc/client";
 import { ChatInput, Messages } from "@/components/chat";
 import { ChevronLeft, Loader2, XCircle } from "lucide-react";
 import { buttonVariants } from "@/components/ui/Button";
+import { ChatContextProvider } from "@/context/chat";
 
 interface ChatWrapperProps {
   fileId: string;
@@ -84,13 +85,15 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
     );
 
   return (
-    <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
-      <div className='flex-1 justify-between flex flex-col mb-28'>
-        <Messages />
-      </div>
+    <ChatContextProvider fileId={fileId}>
+      <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+        <div className='flex-1 justify-between flex flex-col mb-28'>
+          <Messages />
+        </div>
 
-      <ChatInput />
-    </div>
+        <ChatInput />
+      </div>
+    </ChatContextProvider>
   );
 };
 
