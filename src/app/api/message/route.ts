e22aff8e537a334f60fe1f028825a950599282de
Nabarun <nicknabarun@gmail.com>
2023-12-06 23:16:16 +0530

    Implement OpenAI and Langchain APIs for message processing

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,6 +1,11 @@
 import { db } from "@/db";
+import { openai } from "@/lib/openai";
 import { NextRequest } from "next/server";
+import { pinecone } from "@/lib/pinecone";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 import { MessageValidator } from "@/lib/validators/message";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export const POST = async (req: NextRequest) => {
@@ -36,5 +41,84 @@ export const POST = async (req: NextRequest) => {
     },
   });
 
-  // TODO: implement OpenAI and Langchain APIs
+  // 1: vectorize message
+  const pineconeIndex = pinecone.Index("documon");
+
+  const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+
+  // 2: search vector store for the most relevant PDF page to the message
+  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex,
+    namespace: file.id,
+  });
+
+  const results = await vectorStore.similaritySearch(message, 4);
+
+  // 3: access chat history
+  const prevMessages = await db.message.findMany({
+    where: {
+      fileId,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+    take: 6,
+  });
+
+  // 4: making the message structure OpenAI ready
+  const formattedPrevMessages = prevMessages.map((msg) => ({
+    role: msg.isUserMessage ? ("user" as const) : ("assistant" as const),
+    content: msg.text,
+  }));
+
+  // 5: interaction with OpenAI LLM
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+    stream: true,
+    messages: [
+      {
+        role: "system",
+        content:
+          "Use the following pieces of context (or previous conversation if needed) to answer the user's question in markdown format.",
+      },
+      {
+        role: "user",
+        content: `Use the following pieces of context (or previous conversation if needed) to answer the user's question in markdown format. \nIf you don't know the answer, just say that you don't know, refrain from making up an answer.
+        
+        \n----------------\n
+        
+        PREVIOUS CONVERSATION:
+        ${formattedPrevMessages.map((message) => {
+          if (message.role === "user") return `User: ${message.content}\n`;
+          return `Assistant: ${message.content}\n`;
+        })}
+        
+        \n----------------\n
+        
+        CONTEXT:
+        ${results.map((r) => r.pageContent).join("\n\n")}
+        
+        USER INPUT: ${message}`,
+      },
+    ],
+  });
+
+  // what we are doing here is the main reason why a custom API route has been preferred over tRPC in this case
+  const stream = OpenAIStream(response, {
+    async onCompletion(completion) {
+      await db.message.create({
+        data: {
+          text: completion,
+          isUserMessage: false,
+          userId,
+          fileId,
+        },
+      });
+    },
+  });
+
+  return new StreamingTextResponse(stream);
 };
