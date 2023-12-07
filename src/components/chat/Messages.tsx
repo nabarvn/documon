40744511c386d58641760e45c5cdffb33223f3c5
Nabarun <nicknabarun@gmail.com>
2023-12-07 15:27:19 +0530

    Use trpc to fetch file messages in `Messages` component

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,4 +1,25 @@
-const Messages = () => {
+"use client";
+
+import { trpc } from "@/app/_trpc/client";
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
+
+interface MessagesProps {
+  fileId: string;
+}
+
+const Messages = ({ fileId }: MessagesProps) => {
+  const { data, isLoading, fetchNextPage } =
+    trpc.getFileMessages.useInfiniteQuery(
+      {
+        fileId,
+        limit: INFINITE_QUERY_LIMIT,
+      },
+      {
+        getNextPageParam: (lastPage) => lastPage?.nextCursor,
+        keepPreviousData: true,
+      }
+    );
+
   return <div>Messages</div>;
 };
 
