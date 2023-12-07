1d170f0723b847e0fc9db5f91b43d714fb71d1d3
Nabarun <nicknabarun@gmail.com>
2023-12-07 15:22:34 +0530

    Refactor `ChatWrapper` to pass `fileId` prop to `Messages` component

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -88,7 +88,7 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
     <ChatContextProvider fileId={fileId}>
       <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
         <div className='flex-1 justify-between flex flex-col mb-28'>
-          <Messages />
+          <Messages fileId={fileId} />
         </div>
 
         <ChatInput />
