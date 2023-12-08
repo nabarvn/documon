70bfad39b2d95779fe9cda0f7f4275335bee1909
Nabarun <nicknabarun@gmail.com>
2023-12-08 23:49:37 +0530

    Refactor chat component exports to include `Message` component

--- src/components/chat/index.ts
+++ src/components/chat/index.ts
@@ -1,3 +1,4 @@
 export { default as ChatWrapper } from "./ChatWrapper";
 export { default as Messages } from "./Messages";
 export { default as ChatInput } from "./ChatInput";
+export { default as Message } from "./Message";
