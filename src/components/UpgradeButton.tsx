170ced4665e7afb1450ece4aa1b29c108b9795c3
Nabarun <nicknabarun@gmail.com>
2023-12-15 17:28:44 +0530

    Add `UpgradeButton` component for Stripe integration

---
+++ src/components/UpgradeButton.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Button } from "@/components/ui";
+import { ArrowRight } from "lucide-react";
+import { trpc } from "@/app/_trpc/client";
+
+const UpgradeButton = () => {
+  const { mutate: createStripeSession } = trpc.createStripeSession.useMutation({
+    onSuccess: ({ url }) => {
+      window.location.href = url ?? "/dashboard/billing";
+    },
+  });
+
+  return (
+    <Button onClick={() => createStripeSession()} className='w-full'>
+      Upgrade now <ArrowRight className='h-5 w-5 ml-1.5' />
+    </Button>
+  );
+};
+
+export default UpgradeButton;
