ee5e4f3012d6a05540edc0c3195c40d32763a87f
Nabarun <nicknabarun@gmail.com>
2023-12-15 17:31:18 +0530

    Add Stripe billing session creation

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
 import { db } from "@/db";
+import { PLANS } from "@/config/stripe";
 import { TRPCError } from "@trpc/server";
+import { absoluteUrl } from "@/lib/utils";
 import { privateProcedure, publicProcedure, router } from "./trpc";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
+import { getUserSubscriptionPlan, stripe } from "@/lib/stripe";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -33,6 +36,52 @@ export const appRouter = router({
     }
 
     return { success: true };
+  }),
+
+  createStripeSession: privateProcedure.mutation(async ({ ctx }) => {
+    const { userId } = ctx;
+
+    const billingUrl = absoluteUrl("/dashboard/billing");
+
+    if (!userId) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!dbUser) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    const subscriptionPlan = await getUserSubscriptionPlan();
+
+    if (subscriptionPlan.isSubscribed && dbUser.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: dbUser.stripeCustomerId,
+        return_url: billingUrl,
+      });
+
+      return { url: stripeSession.url };
+    }
+
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: billingUrl,
+      cancel_url: billingUrl,
+      payment_method_types: ["card", "paypal"],
+      mode: "subscription",
+      billing_address_collection: "auto",
+      line_items: [
+        {
+          price: PLANS.find((plan) => plan.name === "Pro")?.price.priceIds.test,
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId: userId,
+      },
+    });
+
+    return { url: stripeSession.url };
   }),
 
   getUserFiles: privateProcedure.query(async ({ ctx }) => {
