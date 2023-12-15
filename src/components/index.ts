5389ed155a7354491ffcb43b9fbfe3c1bb07f8df
Nabarun <nicknabarun@gmail.com>
2023-12-15 17:29:38 +0530

    Reorganize component exports and add `UpgradeButton`

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
-export { default as MaxWidthWrapper } from "./MaxWidthWrapper";
+export { Icons } from "./Icons";
 export { default as Navbar } from "./Navbar";
 export { default as Providers } from "./Providers";
 export { default as Dashboard } from "./Dashboard";
 export { default as UploadButton } from "./UploadButton";
 export { default as PdfRenderer } from "./PdfRenderer";
 export { default as PdfFullscreen } from "./PdfFullscreen";
-export { Icons } from "./Icons";
+export { default as UpgradeButton } from "./UpgradeButton";
+export { default as MaxWidthWrapper } from "./MaxWidthWrapper";
