54576ce843b1706e81f19315074f54df73aa531e
Nabarun <nicknabarun@gmail.com>
2023-12-22 23:56:16 +0530

    Add `CodeRenderer` component export to chat module

--- src/components/chat/index.ts
+++ src/components/chat/index.ts
@@ -2,3 +2,4 @@ export { default as ChatWrapper } from "./ChatWrapper";
 export { default as Messages } from "./Messages";
 export { default as ChatInput } from "./ChatInput";
 export { default as Message } from "./Message";
+export { default as CodeRenderer } from "./CodeRenderer";
