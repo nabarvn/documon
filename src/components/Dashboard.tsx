a630ce7cd8fac585b89db651a8b5d96aac6e6c72
Nabarun <nicknabarun@gmail.com>
2023-12-23 00:01:40 +0530

    Refactor `Dashboard` component & PDF file card display

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,48 +1,29 @@
 "use client";
 
-import Link from "next/link";
-import { useState } from "react";
-import { format } from "date-fns";
-import { Button } from "@/components/ui";
+import React from "react";
+import { Ghost } from "lucide-react";
 import { trpc } from "@/app/_trpc/client";
-import UploadButton from "./UploadButton";
 import Skeleton from "react-loading-skeleton";
-import { Ghost, Loader2, MessageSquare, Plus, Trash } from "lucide-react";
+import { getUserSubscriptionPlan } from "@/lib/stripe";
+import { UploadButton, PdfFileCard, MaxWidthWrapper } from "@/components";
 
-const Dashboard = () => {
-  const [currentlyDeletingFile, setCurrentlyDeletingFile] = useState<
-    string | null
-  >(null);
+interface DashboardProps {
+  subscriptionPlan: Awaited<ReturnType<typeof getUserSubscriptionPlan>>;
+}
 
-  const utils = trpc.useContext();
-
+const Dashboard = ({ subscriptionPlan }: DashboardProps) => {
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
 
-  const { mutate: deleteFile } = trpc.deleteFile.useMutation({
-    onSuccess: () => {
-      // force refresh
-      utils.getUserFiles.invalidate();
-    },
-
-    onMutate({ id }) {
-      setCurrentlyDeletingFile(id);
-    },
-
-    onSettled() {
-      setCurrentlyDeletingFile(null);
-    },
-  });
-
   return (
-    <main className='mx-auto max-w-7xl md:p-10'>
+    <MaxWidthWrapper className='max-w-5xl xl:max-w-7xl mb-8 mt-24'>
       <div className='flex flex-col items-start justify-between gap-4 border-b border-gray-200 sm:flex-row sm:items-center sm:gap-0 pb-5 mt-8'>
         <h1 className='font-bold text-5xl text-gray-900 mb-3'>My Files</h1>
-        <UploadButton />
+        <UploadButton isSubscribed={subscriptionPlan.isSubscribed} />
       </div>
 
       {/* display all user files */}
       {files && files?.length !== 0 ? (
-        <ul className='grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3 mt-8'>
+        <ul className='grid grid-cols-1 gap-6 divide-y divide-zinc-200 lg:grid-cols-2 xl:grid-cols-3 mt-8'>
           {files
             .sort(
               (a, b) =>
@@ -50,53 +31,9 @@ const Dashboard = () => {
                 new Date(a.createdAt).getTime()
             )
             .map((file) => (
-              <li
-                key={file.id}
-                className='col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg'
-              >
-                <Link
-                  href={`/dashboard/${file.id}`}
-                  className='flex flex-col gap-2'
-                >
-                  <div className='flex w-full items-center justify-between space-x-6 pt-6 px-6'>
-                    <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500' />
-
-                    <div className='flex-1 truncate'>
-                      <div className='flex items-center space-x-3'>
-                        <h3 className='truncate text-lg font-medium text-zinc-900'>
-                          {file.name}
-                        </h3>
-                      </div>
-                    </div>
-                  </div>
-                </Link>
-
-                <div className='grid grid-cols-3 place-items-center gap-6 text-xs text-zinc-500 px-6 py-2 mt-4'>
-                  <div className='flex items-center gap-2'>
-                    <Plus className='h-4 w-4' />
-                    {format(new Date(file.createdAt), "MMM yyyy")}
-                  </div>
-
-                  <div className='flex items-center gap-2'>
-                    <MessageSquare className='h-4 w-4' />
-                    mocked
-                  </div>
-
-                  <Button
-                    onClick={() => deleteFile({ id: file.id })}
-                    size='sm'
-                    className='flex items-center gap-2 w-full'
-                    variant='destructive'
-                  >
-                    {currentlyDeletingFile === file.id ? (
-                      <Loader2 className='h-4 w-4 animate-spin' />
-                    ) : (
-                      <Trash className='h-4 w-4' />
-                    )}
-                    Delete
-                  </Button>
-                </div>
-              </li>
+              <React.Fragment key={file.id}>
+                <PdfFileCard file={file} />
+              </React.Fragment>
             ))}
         </ul>
       ) : isLoading ? (
@@ -108,7 +45,7 @@ const Dashboard = () => {
           <p>Let&apos;s upload your first PDF.</p>
         </div>
       )}
-    </main>
+    </MaxWidthWrapper>
   );
 };
 
