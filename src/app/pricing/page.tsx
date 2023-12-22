23fd1092dffb4734c6e703478008fe7a6170005d
Nabarun <nicknabarun@gmail.com>
2023-12-22 23:59:42 +0530

    Update pricing page text margins for consistency

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -81,7 +81,7 @@ const PricingPage = () => {
         <div className='mx-auto sm:max-w-lg mb-10'>
           <h1 className='text-6xl font-bold sm:text-7xl'>Pricing</h1>
 
-          <p className='text-gray-600 sm:text-lg mt-5'>
+          <p className='text-gray-600 sm:text-lg mt-5 mx-5 md:mx-0'>
             Whether you&apos;re just trying out our service or need more,
             we&apos;ve got you covered.
           </p>
