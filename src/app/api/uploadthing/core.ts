a3a074bec11104cc4c7e1b882c9ec5c4954195cb
Nabarun <nicknabarun@gmail.com>
2023-12-22 23:58:14 +0530

    Implement subscription-based upload restrictions

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -5,87 +5,136 @@ import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { getUserSubscriptionPlan } from "@/lib/stripe";
+import { PLANS } from "@/config/stripe";
 
 const f = createUploadthing();
 
-// `FileRouter` for the app, can contain multiple file routes
-export const ourFileRouter = {
-  // one can define as many file routes as they would like, each with a unique `routeSlug`
-  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
-    // set permissions and file types for this file route
-    .middleware(() => {
-      const { getUser } = getKindeServerSession();
-      const user = getUser();
+// set permissions and file types for this file route
+const middleware = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
 
-      if (!user || !user.id) throw new Error("Unauthorized");
+  if (!user || !user.id) throw new Error("Unauthorized");
 
-      // whatever is returned here is accessible in `onUploadComplete` as `metadata`
-      return { userId: user.id };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      // this code RUNS ON SERVER after upload
+  const subscriptionPlan = await getUserSubscriptionPlan();
 
-      const createdFile = await db.file.create({
+  // whatever is returned here is accessible in `onUploadComplete` as `metadata`
+  return { subscriptionPlan, userId: user.id };
+};
+
+// this code RUNS ON SERVER after upload
+const onUploadComplete = async ({
+  metadata,
+  file,
+}: {
+  metadata: Awaited<ReturnType<typeof middleware>>;
+  file: {
+    key: string;
+    name: string;
+    url: string;
+  };
+}) => {
+  const isFileExist = await db.file.findFirst({
+    where: {
+      key: file.key,
+    },
+  });
+
+  if (isFileExist) return;
+
+  const createdFile = await db.file.create({
+    data: {
+      key: file.key,
+      name: file.name,
+      userId: metadata.userId,
+      url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+      uploadStatus: "PROCESSING",
+    },
+  });
+
+  try {
+    // to access the PDF file in memory
+    const response = await fetch(
+      `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+    );
+
+    // we need the PDF as blob to be able to index it
+    const blob = await response.blob();
+
+    // loading the PDF into memory
+    const loader = new PDFLoader(blob);
+
+    // extracting the page-level text of the PDF
+    const pageLevelDocs = await loader.load();
+
+    // each document in the array is a page
+    const pagesAmt = pageLevelDocs.length;
+
+    // enforce page limit
+    const {
+      subscriptionPlan: { isSubscribed },
+    } = metadata;
+
+    const isProExceeded =
+      pagesAmt > PLANS.find((plan) => plan.name === "Pro")!.pagesPerPdf;
+
+    const isFreeExceeded =
+      pagesAmt > PLANS.find((plan) => plan.name === "Free")!.pagesPerPdf;
+
+    if ((isSubscribed && isProExceeded) || (!isSubscribed && isFreeExceeded)) {
+      await db.file.update({
         data: {
-          key: file.key,
-          name: file.name,
-          userId: metadata.userId,
-          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-          uploadStatus: "PROCESSING",
+          uploadStatus: "FAILED",
+        },
+        where: {
+          id: createdFile.id,
         },
       });
+    }
 
-      try {
-        // to access the PDF file in memory
-        const response = await fetch(
-          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
-        );
+    // vectorize and index entire document
+    const pineconeIndex = pinecone.Index("documon");
 
-        // we need the PDF as blob to be able to index it
-        const blob = await response.blob();
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+    });
 
-        // loading the PDF into memory
-        const loader = new PDFLoader(blob);
+    await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+      pineconeIndex,
+      namespace: createdFile.id,
+    });
 
-        // extracting the page-level text of the PDF
-        const pageLevelDocs = await loader.load();
+    await db.file.update({
+      data: {
+        uploadStatus: "SUCCESS",
+      },
+      where: {
+        id: createdFile.id,
+      },
+    });
+  } catch (error) {
+    await db.file.update({
+      data: {
+        uploadStatus: "FAILED",
+      },
+      where: {
+        id: createdFile.id,
+      },
+    });
+  }
+};
 
-        // each document in the array is a page
-        const pagesAmt = pageLevelDocs.length;
+// `FileRouter` for the app, can contain multiple file routes
+// one can define as many file routes as they would like, each with a unique `routeSlug`
+export const ourFileRouter = {
+  freePlanUploader: f({ pdf: { maxFileSize: "4MB" } })
+    .middleware(middleware)
+    .onUploadComplete(onUploadComplete),
 
-        // TODO: business logic for subscription plan
-
-        // vectorize and index entire document
-        const pineconeIndex = pinecone.Index("documon");
-
-        const embeddings = new OpenAIEmbeddings({
-          openAIApiKey: process.env.OPENAI_API_KEY,
-        });
-
-        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
-          pineconeIndex,
-          namespace: createdFile.id,
-        });
-
-        await db.file.update({
-          data: {
-            uploadStatus: "SUCCESS",
-          },
-          where: {
-            id: createdFile.id,
-          },
-        });
-      } catch (error) {
-        await db.file.update({
-          data: {
-            uploadStatus: "FAILED",
-          },
-          where: {
-            id: createdFile.id,
-          },
-        });
-      }
-    }),
+  proPlanUploader: f({ pdf: { maxFileSize: "16MB" } })
+    .middleware(middleware)
+    .onUploadComplete(onUploadComplete),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
