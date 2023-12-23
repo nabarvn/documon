c8e78636e781e8ef721833c8129cede877f9056d
Nabarun <nicknabarun@gmail.com>
2023-12-23 22:04:50 +0530

    Enhance mobile menu accessibility

--- src/components/MobileSlideover.tsx
+++ src/components/MobileSlideover.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { useEffect, useRef, useState } from "react";
 
 import {
   ArrowRight,
@@ -14,102 +14,114 @@ import {
 } from "lucide-react";
 
 const MobileSlideover = ({ isAuth }: { isAuth: boolean }) => {
+  const menuRef = useRef<HTMLDivElement>(null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const toggleIsOpen = () => setIsOpen((prev) => !prev);
 
-  const pathname = usePathname();
-
   useEffect(() => {
-    if (isOpen) toggleIsOpen();
-  }, [pathname]);
+    const handleMouseDown = (e: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+        setIsOpen(false);
+      }
+    };
 
-  const closeOnCurrent = (href: string) => {
-    if (pathname === href) {
-      toggleIsOpen();
-    }
-  };
+    document.addEventListener("mousedown", handleMouseDown);
+
+    return () => {
+      document.removeEventListener("mousedown", handleMouseDown);
+    };
+  }, [isOpen]);
 
   return (
-    <div className='sm:hidden'>
-      <Menu
-        onClick={toggleIsOpen}
-        className='relative z-50 h-5 w-5 text-zinc-700'
+    <>
+      <div ref={menuRef} className='sm:hidden'>
+        <Menu
+          onClick={toggleIsOpen}
+          className='relative z-40 h-5 w-5 text-zinc-700'
+        />
+
+        {isOpen ? (
+          <div className='fixed animate-in slide-in-from-top-5 fade-in-20 inset-0 z-30 w-full'>
+            <ul className='absolute bg-white border-b border-zinc-200 shadow-xl grid w-full gap-3 px-10 pt-20 pb-8'>
+              {!isAuth ? (
+                <>
+                  <li>
+                    <Link
+                      href='/sign-up'
+                      onClick={() => setIsOpen(false)}
+                      className='flex items-center w-full font-semibold text-green-600'
+                    >
+                      Get started
+                      <ArrowRight className='h-4 w-4 ml-2' />
+                    </Link>
+                  </li>
+
+                  <li className='h-px w-full bg-gray-300' />
+
+                  <li>
+                    <Link
+                      href='/sign-in'
+                      onClick={() => setIsOpen(false)}
+                      className='flex items-center w-full font-semibold'
+                    >
+                      <LogIn className='h-4 w-4 mr-2' />
+                      Sign in
+                    </Link>
+                  </li>
+
+                  <li className='h-px w-full bg-gray-300' />
+
+                  <li>
+                    <Link
+                      href='/pricing'
+                      onClick={() => setIsOpen(false)}
+                      className='flex items-center w-full font-semibold'
+                    >
+                      <CircleDollarSign className='h-4 w-4 mr-2' />
+                      Pricing
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <Link
+                      href='/dashboard'
+                      onClick={() => setIsOpen(false)}
+                      className='flex items-center w-full font-semibold'
+                    >
+                      <LayoutDashboard className='h-4 w-4 mr-2' />
+                      Dashboard
+                    </Link>
+                  </li>
+
+                  <li className='h-px w-full bg-gray-300' />
+
+                  <li>
+                    <Link
+                      href='/sign-out'
+                      onClick={() => setIsOpen(false)}
+                      className='flex items-center w-full font-semibold'
+                    >
+                      <LogOut className='h-4 w-4 mr-2' />
+                      Sign out
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        ) : null}
+      </div>
+
+      <div
+        onClick={() => setIsOpen(false)}
+        className={cn("md:hidden fixed inset-0 h-screen z-20 bg-gray-900/25", {
+          hidden: !isOpen,
+        })}
       />
-
-      {isOpen ? (
-        <div className='fixed animate-in slide-in-from-top-5 fade-in-20 inset-0 z-0 w-full'>
-          <ul className='absolute bg-white border-b border-zinc-200 shadow-xl grid w-full gap-3 px-10 pt-20 pb-8'>
-            {!isAuth ? (
-              <>
-                <li>
-                  <Link
-                    href='/sign-up'
-                    onClick={() => closeOnCurrent("/sign-up")}
-                    className='flex items-center w-full font-semibold text-green-600'
-                  >
-                    Get started
-                    <ArrowRight className='h-4 w-4 ml-2' />
-                  </Link>
-                </li>
-
-                <li className='h-px w-full bg-gray-300' />
-
-                <li>
-                  <Link
-                    href='/sign-in'
-                    onClick={() => closeOnCurrent("/sign-in")}
-                    className='flex items-center w-full font-semibold'
-                  >
-                    <LogIn className='h-4 w-4 mr-2' />
-                    Sign in
-                  </Link>
-                </li>
-
-                <li className='h-px w-full bg-gray-300' />
-
-                <li>
-                  <Link
-                    href='/pricing'
-                    onClick={() => closeOnCurrent("/pricing")}
-                    className='flex items-center w-full font-semibold'
-                  >
-                    <CircleDollarSign className='h-4 w-4 mr-2' />
-                    Pricing
-                  </Link>
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <Link
-                    href='/dashboard'
-                    onClick={() => closeOnCurrent("/dashboard")}
-                    className='flex items-center w-full font-semibold'
-                  >
-                    <LayoutDashboard className='h-4 w-4 mr-2' />
-                    Dashboard
-                  </Link>
-                </li>
-
-                <li className='h-px w-full bg-gray-300' />
-
-                <li>
-                  <Link
-                    href='/sign-out'
-                    onClick={() => closeOnCurrent("/sign-out")}
-                    className='flex items-center w-full font-semibold'
-                  >
-                    <LogOut className='h-4 w-4 mr-2' />
-                    Sign out
-                  </Link>
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
-      ) : null}
-    </div>
+    </>
   );
 };
 
