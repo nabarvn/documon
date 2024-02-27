e518a72aad1c6b0015a247fd6e12d9eb8dc8ea9b
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-02-27 18:48:48 +0530

    Improve pricing page accessibility and UX

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -83,17 +83,17 @@ const PricingPage = async () => {
 
   return (
     <>
-      <MaxWidthWrapper className='text-center max-w-5xl mb-8 mt-24'>
-        <div className='mx-auto sm:max-w-lg mb-10'>
-          <h1 className='text-6xl font-bold sm:text-7xl'>Pricing</h1>
+      <MaxWidthWrapper className="text-center max-w-5xl mb-8 mt-24">
+        <div className="mx-auto sm:max-w-lg mb-10">
+          <h1 className="text-6xl font-bold sm:text-7xl">Pricing</h1>
 
-          <p className='text-gray-600 sm:text-lg mt-5 mx-5 md:mx-0'>
+          <p className="text-gray-600 sm:text-lg mt-5 mx-5 md:mx-0">
             Whether you&apos;re just trying out our service or need more,
             we&apos;ve got you covered.
           </p>
         </div>
 
-        <div className='grid grid-cols-1 gap-10 lg:grid-cols-2 pt-12'>
+        <div className="grid grid-cols-1 gap-10 lg:grid-cols-2 pt-12">
           <TooltipProvider>
             {pricingItems.map(({ plan, tagline, quota, features }) => {
               const price =
@@ -109,54 +109,54 @@ const PricingPage = async () => {
                   })}
                 >
                   {plan === "Pro" && (
-                    <div className='absolute -top-5 left-0 right-0 mx-auto w-32 rounded-full bg-gradient-to-r from-blue-600 to-cyan-600 text-sm font-medium text-white px-3 py-2'>
+                    <div className="absolute -top-5 left-0 right-0 mx-auto w-32 rounded-full bg-gradient-to-r from-blue-600 to-cyan-600 text-sm font-medium text-white px-3 py-2">
                       Upgrade now
                     </div>
                   )}
 
-                  <div className='p-5'>
-                    <h3 className='text-center font-display text-3xl font-bold my-3'>
+                  <div className="p-5">
+                    <h3 className="text-center font-display text-3xl font-bold my-3">
                       {plan}
                     </h3>
 
-                    <p className='text-gray-500'>{tagline}</p>
+                    <p className="text-gray-500">{tagline}</p>
 
-                    <p className='font-display text-6xl font-semibold my-5'>
+                    <p className="font-display text-6xl font-semibold my-5">
                       ${price}
                     </p>
 
-                    <p className='text-gray-500'>per month</p>
+                    <p className="text-gray-500">per month</p>
                   </div>
 
-                  <div className='flex h-20 items-center justify-center border-b border-t border-gray-200 bg-gray-50'>
-                    <div className='flex items-center space-x-1'>
+                  <div className="flex h-20 items-center justify-center border-b border-t border-gray-200 bg-gray-50">
+                    <div className="flex items-center space-x-1">
                       <p>{quota.toLocaleString()} PDFs/mo included</p>
 
                       <Tooltip delayDuration={300}>
-                        <TooltipTrigger className='cursor-default ml-1.5'>
-                          <HelpCircle className='h-4 w-4 text-zinc-500' />
+                        <TooltipTrigger className="cursor-default ml-1.5">
+                          <HelpCircle className="h-4 w-4 text-zinc-500" />
                         </TooltipTrigger>
 
-                        <TooltipContent className='w-80 p-2'>
+                        <TooltipContent className="w-80 p-2">
                           Number of PDFs you can upload per month.
                         </TooltipContent>
                       </Tooltip>
                     </div>
                   </div>
 
-                  <ul className='space-y-5 my-10 px-8'>
+                  <ul className="space-y-5 my-10 px-8">
                     {features.map(({ text, footnote, negative }) => (
-                      <li key={text} className='flex space-x-5'>
-                        <div className='flex-shrink-0'>
+                      <li key={text} className="flex space-x-5">
+                        <div className="flex-shrink-0">
                           {negative ? (
-                            <Minus className='h-6 w-6 text-gray-300' />
+                            <Minus className="h-6 w-6 text-gray-300" />
                           ) : (
-                            <Check className='h-6 w-6 text-blue-500' />
+                            <Check className="h-6 w-6 text-blue-500" />
                           )}
                         </div>
 
                         {footnote ? (
-                          <div className='flex items-center space-x-1'>
+                          <div className="flex items-center space-x-1">
                             <p
                               className={cn("text-gray-600", {
                                 "text-gray-400": negative,
@@ -166,11 +166,11 @@ const PricingPage = async () => {
                             </p>
 
                             <Tooltip delayDuration={300}>
-                              <TooltipTrigger className='cursor-default ml-1.5'>
-                                <HelpCircle className='h-4 w-4 text-zinc-500' />
+                              <TooltipTrigger className="cursor-default ml-1.5">
+                                <HelpCircle className="h-4 w-4 text-zinc-500" />
                               </TooltipTrigger>
 
-                              <TooltipContent className='w-80 p-2'>
+                              <TooltipContent className="w-80 p-2">
                                 {footnote}
                               </TooltipContent>
                             </Tooltip>
@@ -188,31 +188,31 @@ const PricingPage = async () => {
                     ))}
                   </ul>
 
-                  <div className='border-t border-gray-200' />
+                  <div className="border-t border-gray-200" />
 
-                  <div className='p-5'>
+                  <div className="p-5">
                     {plan === "Free" ? (
                       <Link
-                        href={user ? "/dashboard" : "/sign-in"}
+                        href={user ? "/dashboard/billing" : "/sign-up"}
                         className={buttonVariants({
                           className: "w-full",
                           variant: "secondary",
                         })}
                       >
                         {user ? "Upgrade now" : "Sign up"}
-                        <ArrowRight className='h-5 w-5 ml-1.5' />
+                        <ArrowRight className="h-5 w-5 ml-1.5" />
                       </Link>
                     ) : user ? (
                       <UpgradeButton />
                     ) : (
                       <Link
-                        href='/sign-in'
+                        href="/sign-up"
                         className={buttonVariants({
                           className: "w-full",
                         })}
                       >
                         {user ? "Upgrade now" : "Sign up"}
-                        <ArrowRight className='h-5 w-5 ml-1.5' />
+                        <ArrowRight className="h-5 w-5 ml-1.5" />
                       </Link>
                     )}
                   </div>
