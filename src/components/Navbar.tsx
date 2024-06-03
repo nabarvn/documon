c9fa390c357c932c7d3b58744143746019b6fdb5
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-03 15:49:18 +0530

    Refactor Navbar styling for an improved user interface

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = async () => {
   const plan = await getUserSubscriptionPlan();
 
   return (
-    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all px-4">
+    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="flex z-40 font-semibold">
