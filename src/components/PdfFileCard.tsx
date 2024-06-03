a8b845086b02e5ca2159144184d21b0a10ef9dca
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-03 15:48:02 +0530

    Adjust delete button styling for a cleaner look

--- src/components/PdfFileCard.tsx
+++ src/components/PdfFileCard.tsx
@@ -45,14 +45,14 @@ const PdfFileCard = ({ file }: PdfFileCardProps) => {
   });
 
   return (
-    <li className='col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg'>
-      <Link href={`/dashboard/${file.id}`} className='flex flex-col gap-2'>
-        <div className='flex w-full items-center justify-between space-x-6 pt-6 px-6'>
-          <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500' />
+    <li className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg">
+      <Link href={`/dashboard/${file.id}`} className="flex flex-col gap-2">
+        <div className="flex w-full items-center justify-between space-x-6 pt-6 px-6">
+          <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
 
-          <div className='flex-1 truncate'>
-            <div className='flex items-center space-x-3'>
-              <h3 className='truncate text-lg font-medium text-zinc-900'>
+          <div className="flex-1 truncate">
+            <div className="flex items-center space-x-3">
+              <h3 className="truncate text-lg font-medium text-zinc-900">
                 {file.name}
               </h3>
             </div>
@@ -60,32 +60,32 @@ const PdfFileCard = ({ file }: PdfFileCardProps) => {
         </div>
       </Link>
 
-      <div className='grid grid-cols-3 place-items-center gap-5 text-sm text-zinc-500 px-6 py-2 mt-4'>
-        <div className='flex items-center gap-1'>
-          <Plus className='h-4 w-4' />
+      <div className="grid grid-cols-3 place-items-center gap-5 text-sm text-zinc-500 px-6 py-2 mt-4">
+        <div className="flex items-center gap-1">
+          <Plus className="h-4 w-4" />
           {format(new Date(file.createdAt), "MMM yyyy")}
         </div>
 
-        <div className='flex items-center gap-1'>
-          <MessageSquare className='h-4 w-4' />
+        <div className="flex items-center gap-1">
+          <MessageSquare className="h-4 w-4" />
 
           {data ? (
             data?.messages.length
           ) : (
-            <Loader2 className='h-4 w-4 animate-spin' />
+            <Loader2 className="h-4 w-4 animate-spin" />
           )}
         </div>
 
         <Button
-          size='sm'
+          size="sm"
+          variant="ghost"
           onClick={() => deleteFile({ id: file.id })}
-          className='flex items-center gap-1 w-full'
-          variant='destructive'
+          className="flex items-center text-destructive-foreground hover:text-destructive-foreground hover:bg-destructive gap-1 w-full"
         >
           {currentlyDeletingFile === file.id ? (
-            <Loader2 className='h-4 w-4 animate-spin' />
+            <Loader2 className="h-4 w-4 animate-spin" />
           ) : (
-            <Trash className='h-4 w-4' />
+            <Trash className="h-4 w-4" />
           )}
           Delete
         </Button>
