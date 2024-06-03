e79945c618ac53f9f6a4e6206eec04f61a296e6b
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-03 15:50:50 +0530

    Refactor MaxWidthWrapper styling for better padding consistency

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -10,10 +10,7 @@ const MaxWidthWrapper = ({
 }) => {
   return (
     <div
-      className={cn(
-        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
-        className
-      )}
+      className={cn("mx-auto w-full max-w-screen-xl px-4 md:px-20", className)}
     >
       {children}
     </div>
