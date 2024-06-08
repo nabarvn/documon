e265d072465b59c3a4f371ff2a3336aab4a13f2f
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-08 23:19:30 +0530

    Add LinkRenderer to chat components

--- src/components/chat/index.ts
+++ src/components/chat/index.ts
@@ -3,3 +3,4 @@ export { default as Messages } from "./Messages";
 export { default as ChatInput } from "./ChatInput";
 export { default as Message } from "./Message";
 export { default as CodeRenderer } from "./CodeRenderer";
+export { default as LinkRenderer } from "./LinkRenderer";
