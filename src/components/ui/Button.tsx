1eac5d561f8efbb8aad2fced7f142d72b3a4cab0
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-09-18 18:25:50 +0530

    Refactor ref handling in Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -45,7 +45,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
+        ref={ref as never}
         {...props}
       />
     );
