a880eabb50a32c77dccf0672768d8a62155eec5d
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-09-20 18:33:43 +0530

    Enhance billing page with user session validation

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,7 +1,14 @@
+import { redirect } from "next/navigation";
 import { BillingForm } from "@/components";
 import { getUserSubscriptionPlan } from "@/lib/stripe";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const BillingPage = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
+  if (!user || !user.id) redirect("/auth-callback?origin=dashboard/billing");
+
   const subscriptionPlan = await getUserSubscriptionPlan();
 
   return <BillingForm subscriptionPlan={subscriptionPlan} />;
