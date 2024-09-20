3f9b7643d455bfeb55946cf2137441360d590752
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-09-20 18:34:34 +0530

    Refactor upload route handler for improved configuration

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,8 +1,11 @@
-import { createNextRouteHandler } from "uploadthing/next";
+import { createRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
 
 // export routes for Next App Router
-export const { GET, POST } = createNextRouteHandler({
+export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
+  config: {
+    token: process.env.UPLOADTHING_TOKEN,
+  },
 });
